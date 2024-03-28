0ef5fe9b87d81f06a7b491765a16e0f4329ace22
Villiam <Vjacob19@student.aau.dk>
2024-03-28 12:44:42 +0100

    Installing and removing instrumentation tool

--- run.ps1
+++ run.ps1
@@ -3,8 +3,17 @@
 cd ..
 git clone https://github.com/cs-24-pt-10-01/Using-acorn-to-decorate-JS.git
 
+# install dependencies
+cd Using-acorn-to-decorate-JS
+npm install package.json
+cd ..
+
 # instrumenting
 node Using-acorn-to-decorate-JS/decorateFolder.js ./HotSpotBenchmarkJS
+
+
+# removing instrumentation tool
+remove-item Using-acorn-to-decorate-JS/ -recurse
 
 cd HotSpotBenchmarkJS
 
