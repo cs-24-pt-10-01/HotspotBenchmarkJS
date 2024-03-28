9095166b62ae7753fde5b4ad362a218700a382d2
Villiam <Vjacob19@student.aau.dk>
2024-03-28 11:22:52 +0100

    Benchmarks and a main file

---
+++ Fib.js
@@ -0,0 +1,6 @@
+// test method from Rosetta code
+function fib(n) {
+  return n < 2 ? n : fib(n - 1) + fib(n - 2);
+}
+
+module.exports.fib = fib;
---
+++ main.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+// Inputs
+let toBeSorted = fs.readFileSync("ToBeSorted.json").toString();
+toBeSorted = toBeSorted.replace("[", "").replace("]", "").split(",").map(Number);
+
+const fibInput = 47;
+const nbodyInput = 500000;
+
+// Benchmarks
+const fib = require('./Fib.js').fib;
+const nbody = require('./Nbody.js').N_Body;
+const mergeSortInPlaceFast = require('./MergeSort.js').mergeSortInPlaceFast;
+const sort = require('./QuickSort.js').sort;
+
+// Running benchmarks
+const fibOutput = fib(fibInput);
+const nbodyOutput = nbody(nbodyInput);
+const quickSortOutput = sort(toBeSorted, (a, b) => a < b);
+// MergeSort is in place
+mergeSortInPlaceFast(toBeSorted);
+
+// printing output
+console.log("fib : ", fibOutput);
+console.log("nbody : ", nbodyOutput);
+console.log("mergeSort : ", toBeSorted);
+console.log("quickSort : ", quickSortOutput);
+
