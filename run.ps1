a02a801a0459513969ec723f7867c832b43b97bc
Villiam <Vjacob19@student.aau.dk>
2024-04-17 09:03:54 +0200

    Running full version first in run scripts

--- run.ps1
+++ run.ps1
@@ -1,9 +1,9 @@
 
-# running the different stubbed versions
-node fibStubbed.js
-node mergeSortStubbed.js
-node nbodyStubbed.js
-node quickSortStubbed.js
-
 # running full version (no stubbing)
-node main.js
+node ./main.js
+
+# running the different stubbed versions
+node ./fibStubbed.js
+node ./mergeSortStubbed.js
+node ./nbodyStubbed.js
+node ./quickSortStubbed.js
--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
+# running full version (no stubbing)
+node ./main.js
 
 # running the different stubbed versions
 node ./fibStubbed.js
 node ./mergeSortStubbed.js
 node ./nbodyStubbed.js
 node ./quickSortStubbed.js
-
-# running full version (no stubbing)
-node ./main.js
