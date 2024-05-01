72251cc5003bbee8ea221dbed6d6c61154e064cb
Villiam <Vjacob19@student.aau.dk>
2024-05-01 09:00:41 +0200

    10 repetitions

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
-# running full version (no stubbing)
-node ./main.js
+for i in {1..10}
+do
+    # running full version (no stubbing)
+    node ./main.js
 
-# running the different stubbed versions
-node ./fibStubbed.js
-node ./mergeSortStubbed.js
-node ./nbodyStubbed.js
-node ./quickSortStubbed.js
+    # running the different stubbed versions
+    node ./fibStubbed.js
+    node ./mergeSortStubbed.js
+    node ./nbodyStubbed.js
+    node ./quickSortStubbed.js
+done
