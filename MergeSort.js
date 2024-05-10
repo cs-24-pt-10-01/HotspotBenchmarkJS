159688bf17c99a77cf9497bc8f296ffe1b9b5d34
Mads518d <57893682+Mads518d@users.noreply.github.com>
2024-05-10 13:58:39 +0200

    Added more deep clones

--- MergeSort.js
+++ MergeSort.js
@@ -2,7 +2,7 @@
 function mergeSortInPlaceFast(v) {
 
     let i = 0;
-    let length = 25000; //How many times to create a deep clone (25000 was tested to be slower than fib(47))
+    let length = 30000; //How many times to create a deep clone (30000 was tested to be slower than fib(47))
     let copy = [];
 
     while(i < length){
