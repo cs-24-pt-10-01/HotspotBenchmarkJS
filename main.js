999077e76aba6b4ece0765e5a1913983232f59ba
Jakob <31919330+Jakobzs@users.noreply.github.com>
2024-05-10 10:32:30 +0200

    remove unused stuff

--- main.js
+++ main.js
@@ -9,29 +9,5 @@ const fibWorker = new Worker('./Fib.js', { workerData: fibInput });
 //const mergeSortWorker = new Worker('./MergeSort.js', { workerData: mergeSortInput });
 //const quickSortWorker = new Worker('./QuickSort.js', { workerData: quickSortInput });
 
-// Listening for messages from workers
-fibWorker.once('message', fibOutput => {
-    console.log("fib : ", fibOutput);
-});
-
-/*
-nbodyWorker.once('message', nbodyOutput => {
-    console.log("nbody : ", nbodyOutput);
-});
-
-mergeSortWorker.once('message', mergeSortOutput => {
-    console.log("mergeSort : ", mergeSortOutput);
-});
-
-quickSortWorker.once('message', quickSortOutput => {
-    console.log("quickSort : ", quickSortOutput);
-});
-*/
-
-// Waiting for workers to finish
-fibWorker.on('exit', () => {
-    console.log("fib worker has finished");
-});
-
 // Infinite loop to keep the main thread alive
 //setInterval(() => { }, 1000);
