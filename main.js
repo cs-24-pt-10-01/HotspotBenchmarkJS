bd91c2381c62f4aa0d8c69fbddb0c8c5deda1cdf
Villiam <Vjacob19@student.aau.dk>
2024-05-22 13:57:21 +0200

    Only running Merge for testing

--- main.js
+++ main.js
@@ -11,20 +11,20 @@ const fibInput = 47;
 const nbodyInput = 50000000;
 
 // Benchmarks
-//const fib = require('./Fib.js').fib;
-//const nbody = require('./Nbody.js').N_Body;
+const fib = require('./Fib.js').fib;
+const nbody = require('./Nbody.js').N_Body;
 const mergeSortInPlaceFast = require('./MergeSort.js').mergeSortInPlaceFast;
-//const sort = require('./QuickSort.js').sort;
+const sort = require('./QuickSort.js').sort;
 
 // Running benchmarks
-const fibOutput = fib(fibInput);
-const nbodyOutput = nbody(nbodyInput);
+//const fibOutput = fib(fibInput);
+//const nbodyOutput = nbody(nbodyInput);
 // quickSort and mergeSort are in place
-sort(quickSortInput, (a, b) => a < b);
+//sort(quickSortInput, (a, b) => a < b);
 mergeSortInPlaceFast(mergeSortInput);
 
 // printing output
-console.log("fib : ", fibOutput);
-console.log("nbody : ", nbodyOutput);
-console.log("mergeSort : ", mergeSortInput);
+//console.log("fib : ", fibOutput);
+//console.log("nbody : ", nbodyOutput);
+//console.log("mergeSort : ", mergeSortInput);
 console.log("quickSort : ", quickSortInput);
