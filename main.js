135847711c39dd00e641ae8248ab63138e915dc2
Mads518d <57893682+Mads518d@users.noreply.github.com>
2024-06-17 11:35:27 +0200

    Added Sieve-of-Eratosthenes as benchmark
    The input probably has to be changed

--- main.js
+++ main.js
@@ -9,12 +9,14 @@ quickSortInput = quickSortInput.replace("[", "").replace("]", "").split(",").map
 
 const fibInput = 47;
 const nbodyInput = 50000000;
+const sieveInput = 100000;
 
 // Benchmarks
 const fib = require('./Fib.js').fib;
 const nbody = require('./Nbody.js').N_Body;
 const mergeSortInPlaceFast = require('./MergeSort.js').mergeSortInPlaceFast;
 const sort = require('./QuickSort.js').sort;
+const eratosthenes = require('./sieve-of-eratosthenes-2.js').eratosthenes;
 
 // Running benchmarks
 const fibOutput = fib(fibInput);
@@ -22,9 +24,11 @@ const nbodyOutput = nbody(nbodyInput);
 // quickSort and mergeSort are in place
 sort(quickSortInput, (a, b) => a < b);
 mergeSortInPlaceFast(mergeSortInput);
+const sieveOutput = eratosthenes(sieveInput);
 
 // printing output
 console.log("fib : ", fibOutput);
 console.log("nbody : ", nbodyOutput);
 console.log("mergeSort : ", mergeSortInput);
 console.log("quickSort : ", quickSortInput);
+console.log("sieve : ", sieveOutput);
