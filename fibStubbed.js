f1aea53563425cc587a6c315aa4d8486e64067e4
Villiam <Vjacob19@student.aau.dk>
2024-06-18 12:41:04 +0200

    Updated stubbed versions with new benchmarks

--- fibStubbed.js
+++ fibStubbed.js
@@ -17,12 +17,20 @@ mergeSortInput = mergeSortInput.replace("[", "").replace("]", "").split(",").map
 
 const fibInput = 47;
 const nbodyInput = 50000000;
+const sieveInput = 100000;
+const hailstoneInput = 100000;
+const nqueenInput = 10;
+const spectralInput = 7500;
 
 // Benchmarks
 //const fib = require('./Fib.js').fib;
 const nbody = require('./Nbody.js').N_Body;
 const mergeSortInPlaceFast = require('./MergeSort.js').mergeSortInPlaceFast;
 const sort = require('./QuickSort.js').sort;
+const eratosthenes = require('./sieve-of-eratosthenes-2.js').eratosthenes;
+const hailstone = require('./Hailstone-Sequence.js').LongestSequenceHailstone;
+const nqueen = require('./N-Queens.js').queenPuzzle;
+const spectral = require('./Spectral-norm.js').mainThread;
 
 // Running benchmarks
 const fibOutput = stubbed(fibInput);
@@ -30,11 +38,19 @@ const nbodyOutput = nbody(nbodyInput);
 const quickSortOutput = sort(quickSortInput, (a, b) => a < b);
 // MergeSort is in place
 mergeSortInPlaceFast(mergeSortInput);
+const sieveOutput = eratosthenes(sieveInput);
+const hailstoneOutput = hailstone(hailstoneInput);
+const nqueenOutput = nqueen(nqueenInput, nqueenInput); //N-Queen requires a grid size as input. 'N x N' is used here but 'N x M' can also be used.
+const spectralOutput = spectral(spectralInput);
 
 // printing output
 console.log("fib : ", fibOutput);
 console.log("nbody : ", nbodyOutput);
 console.log("mergeSort : ", mergeSortInput);
 console.log("quickSort : ", quickSortOutput);
+console.log("sieve : ", sieveOutput);
+console.log("hailstone : ", hailstoneOutput);
+console.log("nqueen : ", nqueenOutput);
+console.log("spectral : ", spectralOutput);
 
 rapl.stop("fibStubbed");
